29ea287f00df986bdfd2f1d293d4b191cd83e1f0
hanwatekomal842-lab <hanwatekomal842@gmail.com>
2026-06-27 22:46:04 +0530

    build initial app UI with splash and home screen

--- lib/feature/splash_screen/splash_screen.dart
+++ lib/feature/splash_screen/splash_screen.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:karamchaari/feature/home/presentation/pages/home_page.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const HomePage(),
+        ),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
